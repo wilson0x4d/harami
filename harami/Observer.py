170ce78d31c2a77750552e63a19a5182633b6ef3
wilson0x4d <mrshaunwilson@msn.com>
2025-08-11 23:46:18 -0500

    fix(types): `Observer` supports coro sig

--- harami/Observer.py
+++ harami/Observer.py
@@ -1,7 +1,8 @@
 # SPDX-FileCopyrightText: Copyright (C) Shaun Wilson
 # SPDX-License-Identifier: MIT
 
+from types import CoroutineType
 from typing import Any, Callable
 
 
-type Observer = Callable[[Any], None]
+type Observer = Callable[[Any], None|CoroutineType[Any,Any,None]]
